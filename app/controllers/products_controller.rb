25d2dda3f78b7cf9e080c44791e8f8bec350fc2f
lucerogiron <gironlucero98@gmail.com>
2023-03-01 13:04:07 -0800

    started adding single route to see all products one at a time

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,32 @@
 class ProductsController < ApplicationController
-  def product1
-    @product = Product.first
+  # def product1
+  #   @product = Product.first
+  #   render template: "products/show"
+  # end
+
+  # def product2
+  #   @product = Product.second
+  #   render template: "products/show"
+  # end
+
+  # def product3
+  #   @product = Product.third
+  #   render template: "products/show"
+  # end
+
+  # def product4
+  #   @product = Product.last
+  #   render template: "products/show"
+  # end
+
+  # def all_products
+  #   @products = Product.all
+  #   render template: "products/index"
+  # end
+
+  def product
+    @product = Product.find_by(:id param[id:])
     render template: "products/show"
   end
 
-  def product2
-    @product = Product.second
-    render template: "products/show"
-  end
-
-  def product3
-    @product = Product.third
-    render template: "products/show"
-  end
-
-  def product4
-    @product = Product.last
-    render template: "products/show"
-  end
-
-  def all_products
-    @products = Product.all
-    render template: "products/index"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get "/products_path", controller: "products", action: "all_products"
+  # get "/products_path", controller: "products", action: "all_products"
 
-  get "/product1", controller: "products", action: "product1"
+  # get "/product1", controller: "products", action: "product1"
 
-  get "/product2", controller: "products", action: "product2"
+  # get "/product2", controller: "products", action: "product2"
 
-  get "/product3", controller: "products", action: "product3"
+  # get "/product3", controller: "products", action: "product3"
 
-  get "/product4", controller: "products", action: "product4"
+  # get "/product4", controller: "products", action: "product4"
+
+  get "/product", controller: "products", action: "product"
 end

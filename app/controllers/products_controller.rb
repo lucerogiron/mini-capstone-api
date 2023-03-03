d7d95d603b0b52b3b1e6632e2722cf908a61e6ea
lucerogiron <gironlucero98@gmail.com>
2023-03-03 09:49:54 -0800

    added  update to contorller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,11 +12,22 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(
       name: params[:name],
-      price: params[:name],
-      image_url: params[:name],
-      description: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
     )
     @product.save
+    render :show
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update{
+      name: params[:name] || @product.name,
+      price: params[:price] || @product.price,
+      image_url: params[:image_url] || @product.image_url,
+      description: params[:description] || @product.description,
+    }
     render :show
   end
 end

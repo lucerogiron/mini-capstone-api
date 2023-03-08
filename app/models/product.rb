860cc3701f60893233faffb31015d1bb708e53f4
lucerogiron <gironlucero98@gmail.com>
2023-03-08 10:24:04 -0800

    added supplier model and seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
+  # validates :name, presence: true
+  # validates :name, uniqueness: true
+  # validates :price, presence: true
+  # validates :price, numerically: { greater_than: 0 }
+  # validates :description, presence: true
+  # validates :description, length: { minimun: 10, maximun: 100 }
+  # validates :quantity, numerically: { greater_than: 0 }
+  # validates :quantity, numerically: true
+
   def is_discounted?
     if price <= 10
       return true
---
+++ app/models/supplier.rb
@@ -0,0 +1,5 @@
+class Supplier < ApplicationRecord
+  Supplier.create(name: "Dice Inc", email: "sjgdkh@example.com", phone_number: "1234567890")
+  Supplier.create(name: "D&D Customs", email: "sgdkuahK@example.com", phone_number: "8314567654")
+  Supplier.create(name: "MiniForge", email: "shdhkahw@example.com", phone_number: "2098764563")
+end

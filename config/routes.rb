b16757a5498d728781cbe0c2b0da0133bd74e1ef
lucerogiron <gironlucero98@gmail.com>
2023-03-10 12:15:58 -0800

    added orders controller/test/model/routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
 
   post "/sessions" => "sessions#create"
+
+  post "/orders" => "orders#create"
+
+  # get "/orders" => "orders"
 end

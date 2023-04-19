c51acab50e289790e0e1806c25f02d287ec2a513
lucerogiron <gironlucero98@gmail.com>
2023-04-19 11:54:45 -0700

    added image column to products'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_171940) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_184139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_171940) do
     t.datetime "updated_at", null: false
     t.integer "quantity"
     t.integer "supplier_id"
+    t.integer "image_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
